199887b331646fd5c99eec13ee70647d000ccc41
Filipe Kiss <hello@filipekiss.com.br>
2021-06-30 15:59:04 +0200

    feat(bin): add home manager installer

--- bin/install-nix.sh
+++ bin/install-nix.sh
@@ -3,6 +3,7 @@
 NIX_INSTALLER_URL=https://nixos.org/nix/install
 NIX_DARWIN_URL=https://github.com/LnL7/nix-darwin/archive/master.tar.gz
 
+# TODO: Better condition handling and friendly error messages
 if [[ $(uname -s) == 'Darwin' ]]; then
 	# Check if nix needs to be installed
 	if [[ $+commands[nix-build] -lt 1 ]]; then
@@ -23,5 +24,12 @@ if [[ $(uname -s) == 'Darwin' ]]; then
 		./result/bin/darwin-installer
 		# make just installed `darwin-rebuild` command available in current script
 		[ -f /etc/static/bashrc ] && source /etc/static/bashrc
+	fi
+	# Install home-manager
+	# TODO: find a way to validate if home manager is installed
+	if [[ $+commands[nix-channel] ]]; then
+		nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+		nix-channel --update
+		NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH nix-shell '<home-manager>' -A install
 	fi
 fi
