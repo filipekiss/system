81061f90edf9864731e55afbfc248a167f8e1261
Filipe Kiss <hello@filipekiss.com.br>
2021-06-30 14:15:02 +0200

    feat(bin): add nix-darwin installation

--- bin/install-nix.sh
+++ bin/install-nix.sh
@@ -1,8 +1,27 @@
 #! /usr/bin/env zsh
 
 NIX_INSTALLER_URL=https://nixos.org/nix/install
+NIX_DARWIN_URL=https://github.com/LnL7/nix-darwin/archive/master.tar.gz
 
 if [[ $(uname -s) == 'Darwin' ]]; then
-https://nixos.org/manual/nix/stable/#sect-macos-installation
-  sh <(curl -L $NIX_INSTALLER_URL) --daemon --darwin-use-unencrypted-nix-store-volume
+	# Check if nix needs to be installed
+	if [[ $+commands[nix-build] -lt 1 ]]; then
+		echo "Installing nix..."
+		# See https://nixos.org/manual/nix/stable/#sect-macos-installation
+		sh <(curl -s -L $NIX_INSTALLER_URL) --daemon --darwin-use-unencrypted-nix-store-volume
+	fi
+	# Try to load nix into shell and also acts as validation that nix has been properly installed
+	if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+		source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+	else
+		echo "Failed to initiate nix daemon. Please, install nix and try again."
+	fi
+	# Install nix-darwin
+	if [[ $+commands[darwin-rebuild] -lt 1 ]]; then
+		echo "Installing nix-darwin..."
+		nix-build $NIX_DARWIN_URL -A installer
+		./result/bin/darwin-installer
+		# make just installed `darwin-rebuild` command available in current script
+		[ -f /etc/static/bashrc ] && source /etc/static/bashrc
+	fi
 fi
